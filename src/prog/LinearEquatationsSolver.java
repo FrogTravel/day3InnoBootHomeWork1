cc40fb07e223a6caabae3a0c10be5dddbe432697
ekaterina <katya.levchenko.98@gmail.com>
2016-08-06 19:50:06 +0300

    some new classes

--- src/prog/LinearEquatationsSolver.java
+++ src/prog/LinearEquatationsSolver.java
@@ -3,15 +3,18 @@ package prog;
 /**
  * Created by ekaterina on 04.08.16.
  */
-public class ExtraTask {
+public class LinearEquatationsSolver {
 
+    /**
+     * Copy of matrix is needed to right work of function
+     * Another way method will change matrix array
+     * @param args
+     */
     public static void main(String[] args) {
         double[][] matrix = {{1,2,3},
                             {4,6,3},
                             {2,3,1}};
-        double[][] copyOfMatrix = {{1,2,3},
-                                {4,6,3},
-                                {2,3,1}};
+        double[][] copyOfMatrix = copyMatrix(matrix);
         double[] freeMember =  {6,
                                 8,
                                 12};
@@ -26,6 +29,15 @@ public class ExtraTask {
         }
     }
 
+    /**
+     * Method gives determinant using matrix as param
+     * It iterates through the matrix, than if see that element is
+     * not at the main diagonal first count coefficient
+     * on what non-zero element will be added
+     * than it goes to the right and changes every element in the stroke
+     * @param localMatrix matrix that must be changed
+     * @return determinant of matrix
+     */
     public static double getDeterminate(double[][] localMatrix) {
         double coef = 0;
         for(int i = 0; i < localMatrix.length; i++){
@@ -44,8 +56,6 @@ public class ExtraTask {
                 for(int k = i; k < localMatrix.length; k++){
                     localMatrix[k][j] += coef * localMatrix[k][i];
                 }
-
-
             }
         }
 
@@ -58,6 +68,13 @@ public class ExtraTask {
         return determinant;
     }
 
+    /**
+     * Change one column of matrix to another
+     * @param matrix that must be changed
+     * @param freeMember on what it will be change
+     * @param index what column to change
+     * @return new matrix
+     */
     public static double[][] changeArray(double[][] matrix, double[] freeMember, int index){
         double[][] newMatrix = new double[matrix.length][matrix.length];
         for(int i = 0; i < matrix.length; i++){
@@ -72,6 +89,25 @@ public class ExtraTask {
             //System.out.println();
         }
         return newMatrix;
+    }
+
+
+    /**
+     * Copy matrix
+     * @param matrix that must be copied
+     * @return new matrix
+     */
+    public static double[][] copyMatrix(double matrix[][]){
+        double resultMatrix[][] = new double[matrix.length][matrix.length];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix.length; j++){
+                resultMatrix[i][j] = matrix[i][j];
+                System.out.print(resultMatrix);
+            }
+            System.out.println();
+        }
+        return resultMatrix;
+
     }
 
 
--- src/prog/firstClass.java
+++ src/prog/firstClass.java
@@ -1,31 +1,11 @@
 package prog;
 
-import java.util.Scanner;
-
 /**
- * Created by ekaterina on 03.08.16.
+ * Created by ekaterina on 04.08.16.
  */
 public class firstClass {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-
-        double counter = scan.nextInt();
-
-        double sum = 0;
-
-        /*for(int i = 0; i < counter; i++){
-            sum += (Math.pow(-1, i))/(2*i + 1);
-        }*/
-
-        int i = 0;
-        while(i < counter) {
-            sum += (Math.pow(-1, i))/(2*i + 1);
-            i++;
-        }
-
-        sum *= 4;
-
-        System.out.print(sum);
-
+    public static void main(String[] args) {
+        System.out.print(10_000_000_00f + 9f);
     }
+
 }
